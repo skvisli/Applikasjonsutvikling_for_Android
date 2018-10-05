cf980ed523899a7c56d0f0e091d211fe7809116a
Unknown <skvisli@gmail.com>
2018-10-05 13:39:14 +0200

    Finished øving 1

---
+++ oving1/app/src/main/java/com/example/oving1/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.oving1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        TextView textView = findViewById(R.id.textView);
+        String text = item.getTitle().toString();
+        Log.i(TAG, text);
+        textView.setText(text);
+
+        return super.onOptionsItemSelected(item);
+    }
+}
