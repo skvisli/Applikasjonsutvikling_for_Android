2dd0c207c7e3f0df789af57e512445973ac26f71
Unknown <skvisli@gmail.com>
2018-11-18 23:39:12 +0100

    Code cleanup

--- Hangman/app/src/main/java/com/example/sondre/hangman/SharedViewModel.java
+++ Hangman/app/src/main/java/com/example/sondre/hangman/SharedViewModel.java
@@ -2,29 +2,12 @@ package com.example.sondre.hangman;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
-import android.util.Log;
 
 import java.util.HashMap;
 
 public class SharedViewModel extends ViewModel {
     private MutableLiveData<HashMap> alphabetList = new MutableLiveData<>();
     private HashMap map = new HashMap();
-
-    public SharedViewModel() {
-        char ch;
-        for (ch = 'a'; ch <= 'z'; ch++) {
-            map.put(ch, false);
-        }
-        alphabetList.setValue(map);
-    }
-
-    public MutableLiveData<HashMap> getAlphabetList() {
-        return alphabetList;
-    }
-
-    public void setKeyPressed(char key) {
-        map.put(key, true);
-        alphabetList.postValue(map);
-    }
+    //TODO: Implement ViewModel
 
 }
--- Hangman/app/src/main/java/com/example/sondre/hangman/Words.java
+++
@@ -1,5 +0,0 @@
-package com.example.sondre.hangman;
-
-public class Words {
-
-}
